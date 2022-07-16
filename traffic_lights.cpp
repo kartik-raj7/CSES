2746af8c9f2940ab18f48caa689e15bcd7ef985c
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-07-17 00:04:44 +0530

    update

---
+++ traffic_lights.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <stdlib.h>
+#include <set>
+#include <limits>
+#include <iomanip>
+#define pb push_back
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test ll t; cin>>t; while(t--)
+#define ll long long
+#define loop(i,l,h) for(ll i=(l);i<=(h);i++)
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int c = n+1;
+    unordered_set<int> s;
+    for(int i=0;i<m;i++){
+          
+    }
+}
